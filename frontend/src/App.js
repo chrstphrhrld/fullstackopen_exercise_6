f6560985cd229cc9490a1240d21ac5cb879e4574
chrstphrhrld <ch195@outlook.de>
2023-05-17 15:35:46 +0200

    initial commit with note app and redux

---
+++ frontend/src/App.js
@@ -0,0 +1,60 @@
+import noteReducer, { createNote, toggleImportanceOf } from './reducers/noteReducer'
+// import { useSelector, useDispatch } from 'react-redux'
+import { createStore } from "redux";
+
+const generateId = () =>
+	Number((Math.random() * 100000).toFixed(0))
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+	type: 'NEW_NOTE',
+	payload: {
+		content: 'the app state is in redux store',
+		important: true,
+		id: 1
+	}
+})
+
+store.dispatch({
+	type: 'NEW_NOTE',
+	payload: {
+		content: 'state changes are made with actions',
+		important: false,
+		id: 2
+	}
+})
+
+const App = () => {
+	const addNote = (event) => {
+		event.preventDefault()
+		const content = event.target.note.value
+		event.target.note.value = ''
+		store.dispatch(createNote(content))
+	}
+
+	const toggleImportance = (id) => {
+		store.dispatch(toggleImportanceOf(id))
+	}
+
+	return (
+		<div>
+			<form onSubmit={ addNote }>
+				<input name="note"/>
+				<button type="submit">add</button>
+			</form>
+			<ul>
+				{ store.getState().map(note =>
+					<li
+						key={ note.id }
+						onClick={ () => toggleImportance(note.id) }
+					>
+						{ note.content } <strong>{ note.important ? 'important' : '' }</strong>
+					</li>
+				) }
+			</ul>
+		</div>
+	)
+}
+
+export default App
