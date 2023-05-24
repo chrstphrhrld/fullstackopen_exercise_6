19570293efd495b10dee233f79de0936a2ded60e
chrstphrhrld <ch195@outlook.de>
2023-05-24 14:55:55 +0200

    so much fun with redux stores

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,10 @@ import { Provider } from 'react-redux'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 
-// const store = createStore(noteReducer)
+const store = createStore(noteReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <Provider store={store}>
-    <App />
-  // </Provider>
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
--- frontend/src/reducers/noteReducer.js
+++ frontend/src/reducers/noteReducer.js
@@ -5,20 +5,20 @@ const noteReducer = (state = [], action) => {
     case 'TOGGLE_IMPORTANCE':
       const id = action.payload.id
       const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
       }
       return state.map(note =>
-        note.id !== id ? note : changedNote 
+          note.id !== id ? note : changedNote
       )
     default:
       return state
-    }
-  } 
+  }
+}
 
 const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+    Number((Math.random() * 1000000).toFixed(0))
 
 export const createNote = (content) => {
   return {
@@ -36,6 +36,6 @@ export const toggleImportanceOf = (id) => {
     type: 'TOGGLE_IMPORTANCE',
     payload: { id }
   }
-}  
+}
 
 export default noteReducer
