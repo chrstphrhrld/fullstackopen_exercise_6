c4fdb91c6754f63a3dce20a3b4a751443a8144b1
chrstphrhrld <ch195@outlook.de>
2023-05-25 11:52:11 +0200

    redux toolit implemented

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from 'react-redux'
 
 import App from './App'
@@ -10,11 +10,12 @@ import filterReducer from './reducers/filterReducer'
 import { createNote } from './reducers/noteReducer'
 import { filterChange } from './reducers/filterReducer'
 
-const reducer = combineReducers({
-	notes: noteReducer,
-	filter: filterReducer
+const store = configureStore({
+	reducer: {
+		notes: noteReducer,
+		filter: filterReducer
+	}
 })
-const store = createStore(reducer)
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(filterChange('IMPORTANT'))
 store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
